17eeda8fde58b18e3ec3208314b3f93118721ce0
Holden Turman <holden.turman1@gmail.com>
2024-10-26 18:12:51 -0600

    Scripture Memorization App

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -5,17 +5,20 @@ class Program
 {
     static void Main(string[] args)
     {
+        // This sets a variable to pick a random number from 1, 101 and put it in a new list
+
        Random randomGenerator = new Random();
        int magicNumber = randomGenerator.Next(1, 101);
 
         int input = -1;
 
-
+        // This while loop keeps running until user guesses random number correctly
         while(input != magicNumber)
         {
             Console.Write("What is your guess? ");
             input = int.Parse(Console.ReadLine());
 
+            // conditionals that compares users guess to random number.
             if(magicNumber > input)
             {
                 Console.WriteLine("Higher");
